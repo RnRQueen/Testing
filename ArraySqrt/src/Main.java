34a617f44108dcbea8e9cafd43894a62ee15414e
RnRQueen <zubik1103@gmail.com>
2022-11-08 00:13:18 +0300

    SQRT of array element

---
+++ ArraySqrt/src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+
+public class Main {
+    static double[] array = {36, 0, 6, 102, -5, 110, 32, -16, 28, 100};
+
+    public static void printArray(double[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.printf("%.2f ", array[i]);
+        }
+        System.out.println();
+
+    }
+
+    public static double[] generateArray(int n) {
+
+        double[] randomNums = new double[n];
+        Random r = new Random();
+
+        for (int i = 0; i < randomNums.length; i++) {
+            randomNums[i] = r.nextDouble() * 100;
+        }
+
+        return randomNums;
+    }
+
+
+    public static double sqrtOfElement(double[] numbers, int k) {
+        if ((k < 0) || (k >= numbers.length)) {
+            System.out.println("Индекс за пределами массива");
+            return -1;
+        } else {
+            double num = numbers[k];
+            if (num < 0) {
+                throw new IllegalStateException("Невозмоно вычислить корень из отрицательного числа!");
+            }
+
+            double temp = Math.sqrt(num);
+            System.out.printf("Корень из %.2f = %.2f", num, temp);
+            return temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        double[] rgnArray = generateArray(5);
+        printArray(rgnArray);
+        sqrtOfElement(rgnArray, 4);
+
+
+    }
+
+}
