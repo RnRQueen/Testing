0af010ad88c288d06c6b92ae298e8c9d996d1a6e
RnRQueen <zubik1103@gmail.com>
2022-11-08 17:38:04 +0300

    Average of 2 elements

--- ArraySqrt/src/Main.java
+++ ArraySqrt/src/Main.java
@@ -1,13 +1,15 @@
-/*Дан массив. Составить программу расчета
- квадратного корня из любого элемента массива*/
+/*Дан массив.
+1. Составить программу расчета
+ квадратного корня из любого элемента массива.
+2. Найти среднее арифметическое двух любых элементов массива*/
 
 import java.util.Random;
 
 public class Main {
-    static double[] array = {36, 0, 6, 102, -5, 110, 32, -16, 28, 100};
+    static double[] array = {36.0, 0.0, 6.0, 102.0, -5.0, 110.0, 32.0, -16.0, 28.0, 100.0};
 
     public static void printArray(double[] array) {
-
+        System.out.println("Задан массив чисел: ");
         for (int i = 0; i < array.length; i++) {
             System.out.printf("%.2f ", array[i]);
         }
@@ -27,6 +29,13 @@ public class Main {
         return randomNums;
     }
 
+    public static double averageCalc(double[] numsArr, int n, int m) {
+        double num1 = numsArr[n];
+        double num2 = numsArr[m];
+        double average = (num1 + num2) / 2;
+        System.out.printf("Среднее арифетическое элементов %f и %f массива =  %f\n", num1, num2, average);
+        return average;
+    }
 
     public static double sqrtOfElement(double[] numbers, int k) {
         if ((k < 0) || (k >= numbers.length)) {
@@ -39,7 +48,7 @@ public class Main {
             }
 
             double temp = Math.sqrt(num);
-            System.out.printf("Корень из %.2f = %.2f\n", num, temp);
+            System.out.printf("Квадратный корень из элемента с индексом %d равного %.2f = %.2f\n", k, num, temp);
             return temp;
         }
     }
@@ -50,7 +59,8 @@ public class Main {
         sqrtOfElement(rgnArray, 4);
         printArray(array);              //static array
         sqrtOfElement(array, 9);
-
+        printArray(array);
+        averageCalc(array, 0, 3);
 
     }
 
